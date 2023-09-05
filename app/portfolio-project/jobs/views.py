9631d194d780ec65a7d5f4662f31f0e0eb272304
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-09-05 17:18:55 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- app/portfolio-project/jobs/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-    # return render(request, 'jobs/home.html')
-    
